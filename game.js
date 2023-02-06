d92352f640d01f7c0978f1b9248591acabec0690
Geena <93643278+GeenaCodfrey@users.noreply.github.com>
2023-02-06 15:57:52 +0800

    Update function game(), add function checkResult()

--- game.js
+++ game.js
@@ -18,19 +18,15 @@ function getPlayerChoice() {
     if (playerChoice === null) {
       continue;
     }
-    const playerChoiceLowercase = playerChoice.toLowerCase;
-    
-    if ((playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") && (playerChoice != null)) {
+    const playerChoiceLowercase = playerChoice.toLowerCase();
+    if (options.includes(playerChoiceLowercase)) {
+      validatedInput === true;
       return playerChoice;
-    }
-    else {
-      return "Please try again.";
     }
   }
 }
 
 // Check result
-/*
 function checkResult(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return "Tie";
@@ -46,32 +42,13 @@ function checkResult(playerSelection, computerSelection) {
     return "Player wins";
   }
 }
-*/
-// Play a single round of Rock Paper Scissors
-/*
-function playRound(playerSelection, computerSelection) {
-  const result = checkResult(playerSelection, computerSelection);
 
-  if (result == "Tie") {
-    return "It's a tie!";
-  }
-  else if (result == "Computer wins") {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  }
-  else {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  }
-}
-*/
+// Play a round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+  if (checkResult(playerSelection, computerSelection) === "Tie") {
     return "It's a tie!";
   }
-  else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
-  ) {
+  else if (checkResult(playerSelection, computerSelection) === "Computer wins") {
     return "You Lose! " + computerSelection + " beats " + playerSelection;
   }
   else {
@@ -80,18 +57,33 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  console.log("Slay");
-  for (let i = 0; i < 5; i++) {
+  let scorePlayer = 0;
+  let scoreComputer = 0;
+  console.log("Let's play Rock Paper Scissors");
+
+  for (let j = 0; j < 5; j++) {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
+
+    if (checkResult(playerSelection, computerSelection) === "Player wins") {
+      scorePlayer++;
+    }
+    else if (checkResult(playerSelection, computerSelection) === "Computer wins") {
+      scoreComputer++;
+    }
+  }
+  console.log("Game Over");
+
+  // Check who won most of the rounds. Winner of the game won most rounds
+  if (scorePlayer > scoreComputer) {
+    console.log("Player wins the Game!");
+  }
+  else if (scoreComputer > scorePlayer) {
+    console.log("Computer wins the Game!");
+  }
+  else {
+    console.log("It's a tie!");
   }
 }
-
 game();
-
-/*
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-*/
